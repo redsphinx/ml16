1adf79567b2b1dfcf803b92f222b5f7baceba6d0
xarion <erdicalli@gmail.com>
2016-12-01 18:18:49 +0100

    iterative improvement for 2 neighborhoods

--- ising_vs_anneal/optimizer.m
+++ ising_vs_anneal/optimizer.m
@@ -5,18 +5,14 @@ clear
 clc
 makedata
 METHOD='iter';
-NEIGHBORHOODSIZE=2;
-n_restart =100;
-rep = 30;
-arrep = zeros(1,rep);
-eminar = zeros(1,rep);
+NEIGHBORHOODSIZE = 1;
+n_restart = 100;
 
-a = 0
 switch METHOD,
 case 'iter'
     E_a = zeros(1,n_restart);
     E_min = 1000;
-    parfor t=1:n_restart
+    for t=1:n_restart
         % initialize
         x = 2*(rand(1,n)>0.5)-1;
 % 		E1 = E(x,w);
@@ -29,7 +25,7 @@ case 'iter'
                 % compute dE directly instead of subtracting E's of
                 % different states because of efficiency
                 for i=1:length(x)
-                    fx = x(i) * ( w(i,:)*x' + w(:,i)*x);
+                    fx = -(x(i) * ( w(i,:)*x' + w(:,i)*x));
                     if fx > 0
                         x(i) = -x(i);
                         flag = 1;
@@ -38,9 +34,11 @@ case 'iter'
             case 2
                 % choose new x by flipping bits i,j
                 for i=1:length(x)
-                    for j=1:length(x)
-                        fx = x(i) * ( w(i,:)*x' + w(:,i)*x) + x(j) * ( w(j,:)*x' + w(:,j)*x)...
-                            - w(i,j)*x(i)*x(j) - w(j,i)*x(i)*x(j);
+                    for j=i+1:length(x)
+                        a = x;
+                        a(i) = -x(i);
+                        a(j) = -x(j);
+                        fx = E(x,w) - E(a,w);
                         if fx > 0
                             x(i) = -x(i);
                             x(j) = -x(j);
@@ -51,24 +49,8 @@ case 'iter'
             end;
         end;
         E1 = E(x,w);
-        a = a + 1;
-        a
-% 		E_min = min(E_min,E1);
         E_a(t) = E1;
     end;
-%     E_min = intmax;
-%     for i = 1:size(E_a, 2)
-%         if E_min > E_a(i)
-%             arrep(r) = i ;
-%         end
-%         E_min = min(E_min, E_a(i));
-%         E_a(i) = E_min;
-%     end
-%     eminar(r) = E_min;
-%     figure(r)
-%     plot(E_a)
-%     arrep
-%     eminar
 case 'sa'
 	% initialize
 	x = 2*(rand(1,n)>0.5)-1;
