4ea181bcaa28ff52cbb90aba36a7686066a1dc99
redsphinx <gabi.ras@hotmail.com>
2016-12-14 15:29:24 +0100

    fixed plot in ex4, fixed plot shape bug in ex5

--- optimal_control/ex4.m
+++ optimal_control/ex4.m
@@ -1,7 +1,6 @@
 nus = 10:1:20;
 T = 5;
 dt = 0.01;
-legendstrings = {};
 
 for n=1:numel(nus)
 % nu = 0.1;
@@ -19,15 +18,16 @@ for i=1:numel(ts)
     xhist(i) = x;
     uhist(i) = u(x,t);
     
-    dxi = randn(1) * sqrt(nu*dt)
+    dxi = randn(1) * sqrt(nu*dt);
     x = x + uhist(i)*dt + dxi;
 end
 
 % plot(ts, xhist, ts, uhist)
-color = @(i) [0.5+i/20, 0.5-i/20, 0.5-i/20];
+nuspread = 2*(max(nus) - min(nus));
+color = @(i) [0.5+(i-min(nus))/nuspread, 0.5-(i-min(nus))/nuspread, 0.5-(i-min(nus))/nuspread];
 
 
-plot(ts, xhist, 'Color', color(nu - 10))
+plot(ts, xhist, 'Color', color(nu))
 hold on
 
 % hold off
@@ -35,7 +35,15 @@ hold on
 % title(strcat({'Optimal Control, nu='},num2str(nu), {', T='},num2str(T)))
 end
 
+cmap = reshape(color(nus), [numel(nus) 3]);
+colormap(cmap);
+cbh = colorbar();
+set(cbh, 'YTickLabel', char(arrayfun(@num2str, get(cbh, 'YTick') + min(nus)-1, 'UniformOutput', false)));
+ylabel(cbh, 'nu')
+
 plot([0 T], [0 0], '--', 'Color', [0.5 0.5 0.5])
 plot([0 T], [1 1], '--', 'Color', [1 0 0])
 plot([0 T], [-1 -1], '--', 'Color', [1 0 0])
 xlabel('t')
+
+xlim([4 T]);
--- optimal_control/ex5.m
+++ optimal_control/ex5.m
@@ -1,30 +1,21 @@
-Jtable = NaN(9);
-xs = -2:0.5:2;
-vs = -2:0.5:2;
-phihist = NaN(9,9,numtries);
-for ix = 1:numel(xs)
-x = xs(ix);
-for iv = 1:numel(vs)
-v = vs(iv);
-fprintf('.')
-numtries = 100;
-maxhist = NaN(1, numtries);
-phihist = NaN(1, numtries);
-for seed=1:numtries
-% Arena parameters
-xmin = -2;
-xmax = 2;
-T = 100;
+clear
+clc
 
-% Simulation parameters
+% Arena parameters
+valleywidth = 2;
+T = 10;
 dt = 0.1;
-rng(seed);
 
 % Problem parameters
-nu = 10;
+nu = 20;
 g = 1;
 R = 1;
 A = -1;
+
+% Plotting parameters
+numtries = 10;
+xs = -2:0.8:2;
+vs = -2:1:2;
 
 % Relevant quantities
 L = @(x) -1 -0.5*(tanh(2*x + 2) - tanh(2*x - 2));
@@ -32,45 +23,60 @@ Lderiv = @(x) -0.5*(2*sech(2*x + 2).^2 - 2*sech(2 - 2*x).^2);
 Fg = @(x) (-g*Lderiv(x)) ./ sqrt(1 + Lderiv(x).^2);
 
 %{
-xs = xmin:0.01:xmax;
+xs = -valleywidth:0.01:valleywidth;
 plot(xs, L(xs), xs, Lderiv(xs), xs, Fg(xs));
 legend('L', 'L''', 'Fg')
 xlabel('x')
 %}
 
-x = 0.5;
-v = 0;
-ts = 0:dt:T;
+% Prepare measurements
+phihist = NaN(numel(xs), numel(vs), numtries);
 
-xhist = NaN(1, numel(ts));
-vhist = NaN(1, numel(ts));
+for ix = 1:numel(xs)
+    for iv = 1:numel(vs)
+        fprintf('.')
 
-for i=1:numel(ts)
-    t = ts(i);
-    
-    u = 0;
-    dxi = randn(1) * sqrt(nu*dt);
-    
-    xhist(i) = x;
-    vhist(i) = v;
-    
-    x = x + v*dt;
-    v = Fg(x)*dt + u*dt + dxi;
+        maxhist = NaN(1, numtries);
+
+        for seed=1:numtries
+            rng(seed+238765);
+
+            x = xs(ix);
+            v = vs(iv);
+            ts = 0:dt:T;
+
+            xhist = NaN(1, numel(ts));
+            vhist = NaN(1, numel(ts));
+
+            for i=1:numel(ts)
+                t = ts(i);
+
+                u = 0;
+                dxi = randn(1) * sqrt(nu*dt);
+
+                xhist(i) = x;
+                vhist(i) = v;
+
+                x = x + v*dt;
+                v = Fg(x)*dt + u*dt + dxi;
+            end
+
+            %plot(ts, xhist);
+            %legend('x')
+            phihist(ix, iv, seed) = A * (abs(x) > valleywidth); 
+
+            % maxhist(seed) = max(abs(xhist));
+        end
+
+        % boxplot(maxhist)
+    end
 end
 
-%plot(ts, xhist);
-%legend('x')
-phihist(ix, iv, seed) = A * (abs(x) > 2); 
-
-% maxhist(seed) = max(abs(xhist));
-end
-
-% boxplot(maxhist)
 lambda = 0.1 * nu;
-Jtable(ix, iv) = -lambda * log(sum(exp(-phihist(ix, iv, :)/lambda), 3)/numtries);
+Jtable = -lambda * log(sum(exp(-phihist/lambda), 3)/numtries);
 
-end
-end
-
-[X,V] = meshgrid(xs,vs);
+[X,V] = ndgrid(xs,vs);
 surf(X, V, Jtable)
+xlabel('x')
+ylabel('v')
+zlabel('J')
