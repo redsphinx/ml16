a6f25c6095b47b409248d749dae1a3a4dbc597e0
redsphinx <gabi.ras@hotmail.com>
2016-12-14 13:14:41 +0100

    small changes in ex4 for plotting

--- optimal_control/ex4.m
+++ optimal_control/ex4.m
@@ -1,10 +1,13 @@
-nu = 10;
+nus = 10:1:20;
 T = 5;
 dt = 0.01;
+legendstrings = {};
 
+for n=1:numel(nus)
+% nu = 0.1;
+nu =  nus(n);
 u = @(x,t) (tanh(x ./ (nu*(T-t))) - x) ./ (T-t);
-
-rng(0)
+rng(1)
 x = 0;
 ts = 0:dt:T;
 
@@ -16,13 +19,23 @@ for i=1:numel(ts)
     xhist(i) = x;
     uhist(i) = u(x,t);
     
-    dxi = randn(1) * sqrt(nu*dt);
+    dxi = randn(1) * sqrt(nu*dt)
     x = x + uhist(i)*dt + dxi;
 end
 
-plot(ts, xhist, ts, uhist)
+% plot(ts, xhist, ts, uhist)
+color = @(i) [0.5+i/20, 0.5-i/20, 0.5-i/20];
+
+
+plot(ts, xhist, 'Color', color(nu - 10))
 hold on
-plot([0 T], [0 0], '--', 'Color', [0.5 0.5 0.5]);
-hold off
-legend('x', 'u')
+
+% hold off
+% legend('x', 'zero axis', 'targets', 'Location', 'southwest')
+% title(strcat({'Optimal Control, nu='},num2str(nu), {', T='},num2str(T)))
+end
+
+plot([0 T], [0 0], '--', 'Color', [0.5 0.5 0.5])
+plot([0 T], [1 1], '--', 'Color', [1 0 0])
+plot([0 T], [-1 -1], '--', 'Color', [1 0 0])
 xlabel('t')
