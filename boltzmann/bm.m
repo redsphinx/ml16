93fd0ff32d79eeb21ba00f9030e4109c8329f38b
xarion <erdicalli@gmail.com>
2016-12-15 13:58:20 +0100

    added comments on what we did

--- boltzmann/bm.m
+++ boltzmann/bm.m
@@ -13,7 +13,9 @@ training_dataset = randi([0 1], N,T); % NxT
 learning_rate = 0.04;
 
 % calculating clamped statistics
+% <s_i>_c
 clamped_state_expectations = sum(training_dataset, 2)'/T; % 1xN
+% <s_{ij}>_c
 clamped_state_correlation_expectations = training_dataset * training_dataset' / T; % NxN
 
 % array to hold convergence data
@@ -23,23 +25,32 @@ for learning_step = 1:K
     E = (-0.5*(sum(w*training_dataset)+theta*training_dataset));
     Z = sum(exp(-E));
     p_s = exp(-E)/Z; 
-    
+    % calculating free statistics
+    % <s_i>
     free_state_expectations = p_s * training_dataset'; % 1xN
+    % <s_{ij}>
     free_state_correlation_expectations = (training_dataset .* repmat(p_s, N, 1)) * training_dataset';
-    
+    % delta_w = <s_{ij}>_c - <s_{ij}>
     delta_w = clamped_state_correlation_expectations - free_state_correlation_expectations;
+    % set diagonal of delta_w to 0
     delta_w(1:N+1:N*N) = 0;
+    
+    % delta_theta = <s_i>_c - <s_i>
     delta_theta = clamped_state_expectations - free_state_expectations;
     
-    delta_ws(learning_step) = mean(mean(abs(delta_w)));
-    delta_thetas(learning_step) = mean(abs(delta_theta));
+    % apply learning
     w = w + learning_rate * delta_w;
     theta = theta + learning_rate * delta_theta;
+    
+    % store delta values to show convergence
+    delta_ws(learning_step) = mean(mean(abs(delta_w)));
+    delta_thetas(learning_step) = mean(abs(delta_theta));
 end
+
+% hodor
 hold on
 plot(delta_ws, 'r')
 plot(delta_thetas, 'b')
-
 hold off
 xlabel('learning steps')
 legend('\Delta{w}', '\Delta\theta')
