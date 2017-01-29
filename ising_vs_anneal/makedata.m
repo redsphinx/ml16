b9281c76d558fe97a8168d64e18e119ffa9c0401
xarion <erdicalli@gmail.com>
2017-01-29 17:14:00 +0100

    added option to run things multiple times with parfor and save results of duration, retries and minimum energy. to activate the parfor, change the first for to parfor. it doesn't show plots if parfor is active. if need only 1 plot, make the configuration, set the num_retries to 1 and run it.

--- ising_vs_anneal/makedata.m
+++ ising_vs_anneal/makedata.m
@@ -1,8 +1,11 @@
-rand('state',0)
-n=50;
+function w=makedata(n, COUPLING)
 p=n;
 w=sprandsym(n,p);
-% w=(w>0)-(w<0); % this choice defines a frustrated system
-% w=(w>0); % this choice defines a ferromagnetic (easy) system
-% w=w-diag(diag(w));
-w = ones(n) - eye(n);
+switch COUPLING,
+    case 'frustrated' % hard
+        w=(w>=0)-(w<0);
+    case 'ferromagnetic' % easy
+        w=(w>=0);
+end
+
+w=w-diag(diag(w));
